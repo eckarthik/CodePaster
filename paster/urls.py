c0f136c54af472bb38c2efe3d7b0dcf696aabd8b
Karthik <eckarthik39@gmail.com>
2020-02-29 23:48:44 +0530

    Using Class Based Views

--- paster/urls.py
+++ paster/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.create_paste,name="createpaste"),
-    path('signup',views.register_user,name="signup"),
+    path('', views.PasteCreateView.as_view(),name="createpaste"),
+    path('signup',views.RegisterUserView.as_view(),name="signup"),
     path('login',views.login_user,name="login"),
-    path('logout',views.logout_user,name="logout"),
-    path('pastes/<slug:slug>',views.view_paste,name="pastes"),
-    path('profile',views.profile,name="profile"),
-    path('mypastes',views.my_pastes,name="mypastes"),
-    path('download/<slug:slug>',views.download_paste,name="downloadpaste"),
-    path('pastes/<slug:slug>/raw',views.paste_raw,name="rawpaste"),
+    path('logout',views.LogoutView.as_view(),name="logout"),
+    path('pastes/<slug:slug>',views.ViewPaste.as_view(),name="pastes"),
+    path('profile',views.ProfileView.as_view(),name="profile"),
+    path('mypastes',views.MyPastesView.as_view(),name="mypastes"),
+    path('download/<slug:slug>',views.DownloadPasteView.as_view(),name="downloadpaste"),
+    path('pastes/<slug:slug>/raw',views.RawPasteView.as_view(),name="rawpaste"),
     path('pastes/<slug:slug>/clone',views.ClonePasteView.as_view(),name="clonepaste")
 ]
--- paster/views.py
+++ paster/views.py
@@ -3,30 +3,43 @@ from django.contrib.auth import authenticate,login,logout
 from django.http import HttpResponse,Http404
 from paster.models import Paste,Profile
 from .forms import PasteForm,SignUpForm,LoginForm
-import random,string
-from django.views.generic import View,RedirectView,TemplateView
+import random,string,datetime
+from django.views.generic import View,RedirectView,TemplateView,CreateView,UpdateView,DetailView,ListView
 
-# Create your views here.
-def create_paste(request):
-    if request.method == 'POST':
-        form = PasteForm(request.POST)
-        if form.is_valid():
-            paste = form.save(commit=False)
-            if request.user.is_authenticated:
-                paste.user = request.user #Let's add the user details
-            else:
-                paste.user = None
-            paste.slug = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
-            paste.save()
-            return redirect('/pastes/'+paste.slug)
-    else:
-        form = PasteForm()
+# # Create your views here.
 
-    recent_pastes = Paste.objects.order_by('-created_at')[0:5]
-    return render(request,'home.html',{'form':form,'user':request.user,'recent_pastes':recent_pastes})
+class PasteCreateView(CreateView):
+    template_name = "home.html"
+    form_class = PasteForm
+    success_url = "/pastes/"
+    http_method_names = ['get','post']
 
-def register_user(request):
-    if request.method == 'POST':
+    def form_valid(self, form):
+        paste = form.save(commit=False)
+        if self.request.user.is_authenticated:
+            paste.user = self.request.user
+        else:
+            paste.user = None
+        paste.slug = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+        paste.save()
+        self.success_url = self.success_url+paste.slug
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = PasteForm()
+        context['recent_pastes'] = Paste.objects.order_by('-created_at')[0:5]
+        context['user'] = self.request.user
+        return context
+
+class RegisterUserView(CreateView):
+    form_class = SignUpForm
+    template_name = "signup.html"
+    extra_context = {}
+    success_url = "/signup/"
+    http_method_names = ['get','post']
+
+    def post(self, request, *args, **kwargs):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
@@ -34,9 +47,7 @@ def register_user(request):
             return render(request, 'signup.html', {'form': form,'register_success':"success"})
         else:
             return render(request,'signup.html', {'form':form,'register_success':"failure"})
-    else:
-        form = SignUpForm()
-        return render(request, 'signup.html', {'form': form, 'user': request.user})
+
 
 def login_user(request):
     if request.method == 'POST':
@@ -54,69 +65,94 @@ def login_user(request):
         form = LoginForm()
         return render(request, 'login.html', {'form': form, 'user': request.user})
 
-def logout_user(request):
-    logout(request)
-    return redirect('/')
+class LogoutView(RedirectView):
+    url = "/"
 
-def view_paste(request,slug):
-    try:
-        paste = Paste.objects.get(slug__exact=slug)
-    except Paste.DoesNotExist:
-        raise Http404("Sorry! Paste does not exist")
+    def dispatch(self, request, *args, **kwargs):
+        logout(self.request)
+        return super().dispatch(request,*args,**kwargs)
 
-    recent_pastes = Paste.objects.order_by('-created_at')[0:5]
-    can_be_edited = False
-    if request.method == 'GET':
+class ViewPaste(UpdateView):
+    template_name = "paste_view.html"
+    extra_context = {'can_be_edited':False}
+    slug_field = "slug"
+    slug_url_kwarg = "slug"
+    model = Paste
+    fields = '__all__'
+
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+        self.queryset = Paste.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['paste'] = self.object
+        context['recent_pastes'] = Paste.objects.order_by('-created_at')[0:5]
+        context.update(self.extra_context)
+        return context
+    
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=self.queryset)
         if request.user.is_authenticated:
-            if request.user == paste.user:
-                can_be_edited = True
-        #updating the pasteview counters
-        paste.views = paste.views+1
-        paste.save()
-        return render(request,'paste_view.html',{'paste':paste,'can_be_edited':can_be_edited,'recent_pastes':recent_pastes})
-    elif request.method == 'POST':
-        if request.user.is_authenticated and request.user == paste.user:
-            paste.content = request.POST.get('content')
-            paste.save()
-            return render(request,'paste_view.html',{'paste':paste,'can_be_edited':can_be_edited,'paste_edit_success':"success",'recent_pastes':recent_pastes})
-        else:
-            return render(request, 'paste_view.html',
-                          {'paste': paste, 'can_be_edited': can_be_edited,'paste_edit_success':"failure",'recent_pastes': recent_pastes})
+            if request.user == self.object.user:
+                self.extra_context['can_be_edited'] = True
+        # updating the pasteview counters
+        self.object.views = self.object.views+1
+        self.object.save()
+        return self.render_to_response(context=self.get_context_data())
 
-def profile(request):
-    if request.method == "GET":
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=self.queryset)
+        if request.user.is_authenticated and request.user == self.object.user:
+            self.object.content = request.POST.get('content')
+            self.object.created_at = datetime.datetime.now()
+            self.object.save()
+            return self.render_to_response(context=self.get_context_data())
+        else:
+            return self.render_to_response(context=self.get_context_data())
+
+class ProfileView(DetailView):
+    template_name = "profile.html"
+
+    def get(self,request,*args,**kwargs):
         if request.user.is_authenticated:
             profile = Profile.objects.get(user=request.user)
             return render(request,'profile.html',{'profile':profile})
         else:
             return redirect('/')
-    elif request.method == "POST":
+
+    def post(self,request,*args,**kwargs):
         profile = Profile.objects.get(user=request.user)
         profile.bio = request.POST.get('bio')
         profile.location = request.POST.get('location')
         profile.save()
         return render(request,'profile.html',{'profile_saved':True,'profile':profile})
 
-def my_pastes(request):
-    if request.method == "GET":
+class MyPastesView(ListView):
+    template_name = "my_pastes.html"
+    context_object_name = "my_pastes"
+
+    def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            pastes = Paste.objects.filter(user=request.user)
-            return render(request,"my_pastes.html",{'my_pastes':pastes})
-        else:
-            return redirect('/')
+            self.queryset = Paste.objects.filter(user=request.user)
+        return super().dispatch(request,*args,**kwargs)
 
-def download_paste(request,slug):
-    paste = Paste.objects.get(slug=slug)
-    response = HttpResponse(content=paste.content)
-    paste_title = paste.title.replace(" ","_")
-    response['Content-Disposition'] = 'attachment; filename={}.{}'.format(paste_title, "txt")
-    return response
+class DownloadPasteView(View):
 
-def paste_raw(request,slug):
-    paste = Paste.objects.get(slug=slug)
-    response = HttpResponse(content=paste.content)
-    response['Content-Type'] = 'text/plain'
-    return response
+    def get(self, request, *args, **kwargs):
+        paste = Paste.objects.get(slug=self.kwargs['slug'])
+        response = HttpResponse(content=paste.content)
+        paste_title = paste.title.replace(" ","_")
+        response['Content-Disposition'] = 'attachment; filename={}.{}'.format(paste_title, "txt")
+        return response
+
+class RawPasteView(View):
+
+    def get(self, request, *args, **kwargs):
+        paste = Paste.objects.get(slug=self.kwargs['slug'])
+        response = HttpResponse(content=paste.content)
+        response['Content-Type'] = 'text/plain'
+        return response
 
 class ClonePasteView(TemplateView):
     template_name = "home.html"
@@ -130,3 +166,105 @@ class ClonePasteView(TemplateView):
         context['user'] = self.request.user
         context['recent_pastes'] = recent_pastes
         return context
+
+
+
+# def create_paste(request):
+#     if request.method == 'POST':
+#         form = PasteForm(request.POST)
+#         if form.is_valid():
+#             paste = form.save(commit=False)
+#             if request.user.is_authenticated:
+#                 paste.user = request.user #Let's add the user details
+#             else:
+#                 paste.user = None
+#             paste.slug = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+#             paste.save()
+#             return redirect('/pastes/'+paste.slug)
+#     else:
+#         form = PasteForm()
+#
+#     recent_pastes = Paste.objects.order_by('-created_at')[0:5]
+#     return render(request,'home.html',{'form':form,'user':request.user,'recent_pastes':recent_pastes})
+
+
+# def register_user(request):
+#     if request.method == 'POST':
+#         form = SignUpForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             form = SignUpForm()
+#             return render(request, 'signup.html', {'form': form,'register_success':"success"})
+#         else:
+#             return render(request,'signup.html', {'form':form,'register_success':"failure"})
+#     else:
+#         form = SignUpForm()
+#         return render(request, 'signup.html', {'form': form, 'user': request.user})
+
+
+# def logout_user(request):
+#     logout(request)
+#     return redirect('/')
+
+
+# def view_paste(request,slug):
+#     try:
+#         paste = Paste.objects.get(slug__exact=slug)
+#     except Paste.DoesNotExist:
+#         raise Http404("Sorry! Paste does not exist")
+#
+#     recent_pastes = Paste.objects.order_by('-created_at')[0:5]
+#     can_be_edited = False
+#     if request.method == 'GET':
+#         if request.user.is_authenticated:
+#             if request.user == paste.user:
+#                 can_be_edited = True
+#         #updating the pasteview counters
+#         paste.views = paste.views+1
+#         paste.save()
+#         return render(request,'paste_view.html',{'paste':paste,'can_be_edited':can_be_edited,'recent_pastes':recent_pastes})
+#     elif request.method == 'POST':
+#         if request.user.is_authenticated and request.user == paste.user:
+#             paste.content = request.POST.get('content')
+#             paste.save()
+#             return render(request,'paste_view.html',{'paste':paste,'can_be_edited':can_be_edited,'paste_edit_success':"success",'recent_pastes':recent_pastes})
+#         else:
+#             return render(request, 'paste_view.html',
+#                           {'paste': paste, 'can_be_edited': can_be_edited,'paste_edit_success':"failure",'recent_pastes': recent_pastes})
+
+# def profile(request):
+#     if request.method == "GET":
+#         if request.user.is_authenticated:
+#             profile = Profile.objects.get(user=request.user)
+#             return render(request,'profile.html',{'profile':profile})
+#         else:
+#             return redirect('/')
+#     elif request.method == "POST":
+#         profile = Profile.objects.get(user=request.user)
+#         profile.bio = request.POST.get('bio')
+#         profile.location = request.POST.get('location')
+#         profile.save()
+#         return render(request,'profile.html',{'profile_saved':True,'profile':profile})
+
+
+# def my_pastes(request):
+#     if request.method == "GET":
+#         if request.user.is_authenticated:
+#             pastes = Paste.objects.filter(user=request.user)
+#             return render(request,"my_pastes.html",{'my_pastes':pastes})
+#         else:
+#             return redirect('/')
+
+
+# def download_paste(request,slug):
+#     paste = Paste.objects.get(slug=slug)
+#     response = HttpResponse(content=paste.content)
+#     paste_title = paste.title.replace(" ","_")
+#     response['Content-Disposition'] = 'attachment; filename={}.{}'.format(paste_title, "txt")
+#     return response
+
+# def paste_raw(request,slug):
+#     paste = Paste.objects.get(slug=slug)
+#     response = HttpResponse(content=paste.content)
+#     response['Content-Type'] = 'text/plain'
+#     return response
